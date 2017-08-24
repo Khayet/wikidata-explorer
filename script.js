fbfd70ebe5b8a7e139843abaa2b5d5d461e5135f
Khayet <jakob.herpel@uni-weimar.de>
2017-08-24 23:02:49 +0200

    Pass query to getWikidata as argument.

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
 // query wikidata:
 
+// This should display a sample Wikidata response. We can request either xml, or json, i think.
+query = "SELECT ?item ?itemLabel WHERE { ?item wdt:P31 wd:Q146. SERVICE wikibase:label { bd:serviceParam wikibase:language \"en\". } }"
+
 button = document.getElementById("getWikidata")
-button.addEventListener("click", getWikidata)
+button.addEventListener("click", function() { getWikidata(query) })
 
 // See: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
 function logResponse() {
     console.log(this.responseText)
 }
 
- function getWikidata() {
+function displayResponse() {
+    document.getElementById("displayWikidata")
+        .textContent = this.responseText 
+}
+    
+
+// TODO: Make this asynchronous
+function getWikidata(query) {
     console.log("Sending Request")
     let httpRequest = new XMLHttpRequest()
-    httpRequest.addEventListener("load", logResponse)
+    httpRequest.addEventListener("load", displayResponse)
     httpRequest.open(
         "GET", 
-        "https://query.wikidata.org/sparql?query=SELECT" +
-        "?item ?itemLabel WHERE { ?item wdt:P31 wd:Q146. SERVICE wikibase:label" +
-        "{ bd:serviceParam wikibase:language \"en\". } }" )
+        "https://query.wikidata.org/sparql?query=" + query)
     httpRequest.send()
 }
