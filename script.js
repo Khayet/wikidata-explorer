435d37fea5bf1c002552e0f8829f42907d6727b4
Khayet <jakob.herpel@uni-weimar.de>
2017-08-28 18:25:51 +0200

    Construct query during runtime. Currently, we can only choose the entity of which instances should be queried.

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-// query wikidata:
-
-// This should display a sample Wikidata response. We can request either xml, or json, i think.
-query = "SELECT ?item ?itemLabel WHERE { ?item wdt:P31 wd:Q146. SERVICE wikibase:label { bd:serviceParam wikibase:language \"en\". } } LIMIT 10"
-
 button = document.getElementById("getWikidata")
-button.addEventListener("click", function() { getWikidata(query) })
+button.addEventListener("click", function() { getWikidata("wd:Q5") }) // wd:Q146 -> cat
 
-// See: https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Using_XMLHttpRequest
+function constructQueryInstancesOf(entity) {
+    // constructs a query querying all entities that are instances of the passed entity
+    let query =  
+        "SELECT ?item ?itemLabel " + 
+        "WHERE { " +
+        "?item wdt:P31 " + entity + ". " +
+        "SERVICE wikibase:label { bd:serviceParam wikibase:language \"en\". } " +
+        "} " +
+        "LIMIT 10 "
+    console.log(query)
+    return query
+}
+
 function logResponse() {
     console.log(this.responseText)
 }
@@ -19,21 +26,23 @@ function displayResponse() {
 
 function parseResponse() {
     console.log("Parsing response..")
+    console.log(this.responseText)
     response = JSON.parse(this.responseText)
 
     let results = ""
     for (let i = 0; i < response.results.bindings.length; i++) {
-        results += response.results.bindings[i].itemLabel.value + "\n"
+        results += response.results.bindings[i].item.value + ": " + response.results.bindings[i].itemLabel.value + "\n"
     }
 
     document.getElementById("displayWikidata")
-        .textContent += query + "\n\n" + results
+        .textContent += results
 }
     
-function getWikidata(query) {
+function getWikidata(entity) {
     console.log("Sending Request")
+    let query = constructQueryInstancesOf(entity) 
     let httpRequest = new XMLHttpRequest()
-    httpRequest.addEventListener("load", logResponse)
+    httpRequest.addEventListener("load", console.log(this.responseText))
     httpRequest.addEventListener("load", parseResponse)
     httpRequest.open(
         "GET", 
