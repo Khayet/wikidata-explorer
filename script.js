53d5d8407bc458bb8f843dfacd0e411abdefb1fb
Khayet <jakob.herpel@uni-weimar.de>
2017-08-29 20:42:15 +0200

    Remove button. Query wikidata on every page load.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 "use strict"
 
-const button = document.getElementById("getWikidata")
-button.addEventListener("click", function() { getWikidata("wd:Q963") }) // wd:Q146 -> cat
+getWikidata("wd:Q103")
 
 //see: https://stackoverflow.com/questions/25100224/how-to-get-a-list-of-all-wikidata-properties
 function constructQueryPropsAndObjects(entity, limit = 10) {
@@ -47,19 +46,19 @@ function parseResponse(res) {
     console.log("Parsing response..")
     console.log(res)
     const response = JSON.parse(res)
-    const properties = []
-    const objects = []
+    const properties = [], objects = []
+    const propertyLabels = [], objectLabels = []
     const results = response.results.bindings 
     for (let i = 0; i < results.length; i++) {
-        properties.push(results[i].propLabel.value)
-        objects.push(results[i].objectLabel.value)
-        // results += response.results.bindings[i].propLabel.value + "   " + response.results.bindings[i].objectLabel.value + "\n"
-        // results += response.results.bindings[i].item.value + ": " + response.results.bindings[i].itemLabel.value + "\n"
+        properties.push(results[i].prop.value)
+        objects.push(results[i].prop.value)
+        propertyLabels.push(results[i].propLabel.value)
+        objectLabels.push(results[i].objectLabel.value)
     }
 
     for (let i = 0; i < results.length; i++) {
         document.getElementById("displayWikidata")
-            .textContent += properties[i] + " ... " + objects[i] + "\n"
+            .textContent += propertyLabels[i] + " ... " + objectLabels[i] + "\n"
     }
 }
     
